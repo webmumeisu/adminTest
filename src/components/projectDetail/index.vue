ac76ba4f880c46c0e846dd2b91f8fbf4e26c6be8
meisu <meis@tenzont.com>
2023-08-09 21:28:05 +0800

    小修改

---
+++ src/components/projectDetail/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="app-container">
+    我是子组件
+    <h3>获取父组件传参</h3>
+    <div>projectId： {{ projectId }}</div>
+    <div>sexOptions: {{ sexOptions }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BasicPage',
+  components: {},
+  props: {
+    projectId: {
+      type: String,
+      default: ''
+    },
+    sexOptions: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  },
+  data() {
+    return {
+      name: 'vue2',
+      baseURL: import.meta.env.VITE_APP_BASE_API
+    }
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {},
+  methods: {
+    getList() {
+      console.log('getList')
+    },
+    getSonfn() {
+      console.log('父组件调用了子的方法')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.app-container {
+  width: 100%;
+  // background-image: url('../assets/img/underline.png');
+}
+</style>
+
---
+++ src/views/basePage/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="app-container">
+    <!-- class="filter-container" -->
+    基本页面{{ name }}
+    <div @click="handleFnSon">点击我调用子组件方法getSonfn</div>
+    <detail ref="child" :project-id="projectId" :sex-options="sexOptions" />
+  </div>
+</template>
+
+<script>
+import detail from '@/components/projectDetail/index'
+export default {
+  name: 'BasicPage',
+  components: {
+    detail
+  },
+  data() {
+    return {
+      baseURL: import.meta.env.VITE_APP_BASE_API,
+      name: 'vue2',
+      // 子组件传参
+      projectId: '22335566',
+      sexOptions: [
+        { label: '女', value: '0' },
+        { label: '男', value: '1' }
+      ]
+    }
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {},
+  methods: {
+    getList() {
+      console.log('getList')
+    },
+    handleFnSon() {
+      this.$refs.child.getSonfn()
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.app-container {
+  width: 100%;
+  // background-image: url('../assets/img/underline.png');
+}
+</style>
+
